e2053f86c2ccf374b41f10efaa84082e0b2513b2
Denis Onsare <denisonsare@gmail.com>
2022-03-10 07:30:39 +0300

    update script

---
+++ script.js
@@ -0,0 +1,91 @@
+// This will be the client seed of block 637645
+const SALT = '00000000000000000000cd2bcb44f656649c69d8b17ade0399168f3cbe859d26';
+let isVerifying = false;
+$('#game_verify_submit').on('click', () => {
+  if (isVerifying) return;
+  
+  isVerifying = true;
+  $('#game_hash_input').parent().addClass('is-loading');
+  $('#game_verify_submit').addClass('is-loading');
+  $('#game_hash_input, #game_amount_input, #game_verify_submit').attr('disabled', 'disabled');
+  $('#game_verify_table').html('');
+
+	const numberOfGames = $('#game_amount_input').val();
+  let hash = $('#game_hash_input').val();
+  for (let i = 0; i < numberOfGames; i++) {
+    const bust = gameResult(hash);
+      addTableRow(hash, bust, i);
+  	hash = String(CryptoJS.SHA256(hash));
+  }
+});
+
+$('#game_amount_input').on('keyup', () => {
+  if ($('#game_amount_input').val() >= 10000) {
+    if ($('#game_verify_warning').length) return;
+    $('#game_verify_submit').parent().append(
+      $('<span/>').attr({
+        'id': 'game_verify_warning',
+        'class': 'tag is-warning'
+      }).text("Verifying a huge amount of games may consume more ressources from your CPU")
+    );
+  } else {
+    if ($('#game_verify_warning').length) {
+      $('#game_verify_warning').remove();
+    }
+  }
+});
+
+const addTableRow = (hash, bust, index) => {
+  $('<tr/>').attr({
+    'class': index === 0 ? 'is-first' : null
+  }).append(
+    $('<td/>').text(hash)
+  ).append(
+    $('<td/>').text(bust).attr({
+      'class': bust === 1.98 ? 'is-at-median' : bust > 1.98 ? 'is-over-median' : 'is-under-median'
+    })
+  ).appendToWithIndex($('#game_verify_table'), index);
+
+  if (index >= $('#game_amount_input').val() - 1) {
+    $('#game_hash_input').parent().removeClass('is-loading');
+    $('#game_verify_submit').removeClass('is-loading');
+    $('#game_hash_input, #game_amount_input, #game_verify_submit').removeAttr("disabled");
+    isVerifying = false;
+  }
+};
+
+/**
+  * @desc Calculates the game result from its hash
+  * @param binary seed - Hash of the game. Ex.: Buffer.from(seed)
+  * @param string salt - Client seed; A bitcoin block hash
+  * @return number
+*/
+const gameResult = (seed) => {
+  const nBits = 52; // number of most significant bits to use
+
+	// 1. HMAC_SHA256(message=seed, key=salt)
+	const hash = CryptoJS.HmacSHA256(seed, SALT).toString();
+
+	// 2. r = 52 most significant bits
+	const r = parseInt(hash.slice(0, nBits / 4), 16);
+
+	// 3. X = r / 2^52
+	const X = r / Math.pow(2, nBits); // uniformly distributed in [0; 1)
+
+	// 4. X = 90 / (1-X)
+	const result = Math.floor(90 / (1 - X));
+
+	// 5. return capped to 1 on lower end
+	return Math.max(1, result / 100);
+};
+
+$.fn.appendToWithIndex = function(to, index) {
+  if (!to instanceof jQuery) {
+    to = $(to);
+  }
+  if (index === 0) {
+    $(this).prependTo(to);
+  } else {
+    $(this).insertAfter(to.children().eq(index - 1));
+  }
+};
